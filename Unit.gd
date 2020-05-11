51b73d5f90233b822d969d6133679a68b4d87ef8
lordofsraam <lordofsraam@gmail.com>
2020-05-11 17:18:51 -0400

    Unit color changes after moving x amount of spaces

--- Unit.gd
+++ Unit.gd
@@ -1,7 +1,20 @@
 extends Polygon2D
 
 export var tween_speed: float = 0.2
+export var total_moves: int = 20
+
+func update_color():
+	if total_moves <= 0:
+		self.color = Color(1, 0, 0)
 
 func move_to(pos: Vector2):
+	var v_diff = (self.position - pos)
+	
+	var diff = abs(v_diff.x / 64) + abs(v_diff.y / 64)
+	
+	self.total_moves -= diff
+	
 	$Tween.interpolate_property(self, "position", self.position, pos, 0.2, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	$Tween.start()
+	
+	self.update_color()
